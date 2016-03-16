eca88f429000fdbb82aa0c71299954f040458f50
erik--vink <erik.vink95@gmail.com>
2016-03-16 22:10:47 +0100

    Detailview Image + title

--- app/src/main/java/com/example/erik/fragments/DetailFragment.java
+++ app/src/main/java/com/example/erik/fragments/DetailFragment.java
@@ -1,13 +1,20 @@
 package com.example.erik.fragments;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.erik.fragments.old.PokemonModel;
+
+import java.io.InputStream;
 
 public class DetailFragment extends Fragment {
 
@@ -30,14 +37,16 @@ public class DetailFragment extends Fragment {
         View view = getView();
         selectedPokemon = pokemon;
 
+        getActivity().setTitle(pokemon.getName());
+
 //        TextView pokedexNumber = (TextView) view.findViewById(R.id.pokedex_number);
 //        pokedexNumber.setText(pokemon.getPokedexNumber());
         TextView name = (TextView) view.findViewById(R.id.detailName);
         name.setText(pokemon.getName());
 //        TextView type = (TextView) view.findViewById(R.id.type);
 //        type.setText(pokemon.getType());
-//        ImageView image = (ImageView) view.findViewById(R.id.image);
-//        image.setImageBitmap(pokemon.getImage());
+        ImageView image = (ImageView) view.findViewById(R.id.detailImage);
+        new DownloadImageTask(image).execute(pokemon.getImage());
     }
 
     public void setDisplayData(View view){
@@ -50,6 +59,31 @@ public class DetailFragment extends Fragment {
 //        type.setText(pokemon.getType());
 //        ImageView image = (ImageView) view.findViewById(R.id.image);
 //        image.setImageBitmap(pokemon.getImage());
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon = null;
+            try{
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
 
